40bb44ae7e578f1f6c975fecd7dfcd8d52a8a58e
Mingtian Yang <tswymt@gmail.com>
2018-04-20 17:07:03 -0400

    改成杨铭添

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Text>zhao yi ze</Text>
+        <Text>yang ming tian</Text>
       </View>
     );
   }
