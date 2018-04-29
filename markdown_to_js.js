7d3ff238391aaadc94118975fe4a3ed6adc9380f
Mingtian Yang <tswymt@gmail.com>
2018-04-29 14:58:27 -0400

    Revert "update"
    This reverts commit 0fbfa333262e25dfeceb1d9570e2e3f3a73ada7b.

--- markdown_to_js.js
+++ markdown_to_js.js
@@ -2,10 +2,8 @@ var fs = require('fs');
 
 var walkPath = './xxsc_data';
 var fileList = [];
-var fileData = '';
-var fileDataForWWMP = ''; //微信小程序数据格式
-var menuData = {};
-var exportDataForWWMP = 'module.exports = { \n';
+var fileData = ''
+var menuData = {}
 var walk = function (dir, done) {
   fs.readdir(dir, function (error, list) {
     if (error) {
@@ -30,6 +28,7 @@ var walk = function (dir, done) {
             next();
           });
         } else {
+          // do stuff to file here
           fileList.push(file)
           next();
         }
@@ -40,14 +39,11 @@ var walk = function (dir, done) {
 
 var readFiles = function (fileList, done) {
   var i = 0;
+
   (function next () {
     var file = fileList[i++];
 
     if (!file) {
-
-      exportDataForWWMP = exportDataForWWMP.substring(0, exportDataForWWMP.length - 3);
-      exportDataForWWMP = exportDataForWWMP + '}'
-      fileDataForWWMP = fileDataForWWMP + '\n' + exportDataForWWMP
       return done(null);
     }
 
@@ -59,37 +55,23 @@ var readFiles = function (fileList, done) {
       var variableName = file.substring(12).replace(/[\/-]/g, '_')
       variableName = variableName.replace('.md', '')
       fileData = fileData + '\n' + "export const " + variableName +" = ` \n" + data + "`"
-      fileDataForWWMP =  fileDataForWWMP + '\n' + "var " + variableName +" = ` \n" + data + "`"
       menuData[title] = variableName
-      exportDataForWWMP = exportDataForWWMP + "'" + variableName + "':" + variableName + ', \n'
+      console.log(title);
       next();
     });
 
   })();
 }
 
-var writeFile = function () {
+var writeFile = function (fileData, menuData) {
   fs.writeFile("./xxsc_data_js/data.js", fileData, function(err) {
     if (err) {
       return console.log(err);
     }
     console.log("The bookData was saved!");
   });
-  fs.writeFile("./xxsc_data_js/data_wwmp.js", fileDataForWWMP, function(err) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log("The bookData for wechat mini program was saved!");
-  });
   stringMenuData = 'export const menuData =' + JSON.stringify(menuData)
-  stringWechatMenuData = 'module.exports =' + JSON.stringify(menuData)
   fs.writeFile("./xxsc_data_js/menu.js", stringMenuData, function(err) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log("The menuData was saved!");
-  });
-  fs.writeFile("./xxsc_data_js/menu_wxmp.js", stringWechatMenuData, function(err) {
     if (err) {
       return console.log(err);
     }
@@ -118,7 +100,7 @@ walk(walkPath, function(error) {
       if (error) {
         throw error;
       } else {
-        writeFile()
+        writeFile(fileData, menuData)
         console.log('-------------------------------------------------------------');
         console.log('finished. reading');
         console.log('-------------------------------------------------------------');      }
